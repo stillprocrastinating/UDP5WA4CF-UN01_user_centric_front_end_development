56dae8ef7e068a5e720d43490391b6fcb34baefc
stillprocrastinating <elise.o.hitchcock@outlook.com>
2025-05-16 09:07:29 +0100

    Update email functionality

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,3 +1,6 @@
+
+// This JavaScript is no longer required, but is being kept for prosperity of life cycle procedures
+
 function email() {
     alert("Feel free to drop me a message! My email is rosemary.2008@live.co.uk");
 }
